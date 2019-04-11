6afca84aac42e7f3cd372924e206504d6c3aed47
rickaaron <ricardoaaron_@hotmail.com>
2019-04-11 10:53:44 -0500

    30 views

---
+++ src/views/Logout.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class=" ">
+    <div class="columns is-centered is-mobile ">
+      <div class="column is-6 ">
+        <div class=" box has-text-centered has-text-primary ">
+          <h3 class=" title is-2  "> Saliendo </h3>
+            <fa-icon icon='circle-notch' size='6x' spin >
+
+            </fa-icon>
+    
+
+
+        </div>
+
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return ({
+        paciente: {
+          sintomas: 'Dientes chuecos ',
+          tratamiento: ' Radiografia, braquets, endodoncia ',
+          receta: 'Paracettamol 10 tabletas, 1 cada 12 horas. Diprofenaco 10 tabletas, 1 cada 12 horas.',
+        }
+      })
+    }
+  }
+
+</script>
+
+<style>
+
+</style>
